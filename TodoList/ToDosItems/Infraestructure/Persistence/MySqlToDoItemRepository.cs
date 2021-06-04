369aa0d5d12456b9d8de36ca899c45e322adc717
FMPS2199 <1059712@senati.pe>
2021-06-04 10:02:15 -0500

    Implement Save method in MySqlToDoItemRepository

--- TodoList/ToDosItems/Infraestructure/Persistence/MySqlToDoItemRepository.cs
+++ TodoList/ToDosItems/Infraestructure/Persistence/MySqlToDoItemRepository.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TodoList.ToDosItems.Domain;
+using TodoList.ToDosItems.Shared.Infraestrucutre.Persistence.EntityFramework;
 
 namespace TodoList.ToDosItems.Infraestructure.Persistence
 {
     public class MySqlToDoItemRepository : IToDoRepository
     {
-        public Task Save(ToDoItem todoItem)
+        private TodoListContext _conext;
+
+        public MySqlToDoItemRepository(TodoListContext context) {
+            _conext = context;
+        }
+
+        public async Task Save(ToDoItem todoItem)
         {
-            throw new NotImplementedException();
+            await _conext.ToDoItems.AddAsync(todoItem);
+            await _conext.SaveChangesAsync();
         }
 
         public Task<ToDoItem> Search(int id)
