e083eb0395b01db895bd7cc293d65a8f1e872970
FMPS2199 <1059712@senati.pe>
2021-06-04 09:45:35 -0500

    Create TodoItemCreator

---
+++ TodoList/ToDosItems/Application/Create/TodoItemCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.ToDosItems.Domain;
+
+namespace TodoList.ToDosItems.Application.Create
+{
+    public class TodoItemCreator
+    {
+        private readonly IToDoRepository _context;
+
+        public TodoItemCreator(IToDoRepository conext) => _context = conext;
+
+        public void CreateToDoItem(int id, string title, string description, bool isDone) {
+            var todoTitle = new ToDoTitle(title);
+
+            var toDoItem = new ToDoItem(id, todoTitle, description, isDone);
+
+            _context.Save(toDoItem);
+        }
+    }
+}
