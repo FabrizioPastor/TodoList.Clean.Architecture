1957a397e869d6ad954b9f48b6920b1ef569631c
FMPS2199 <1059712@senati.pe>
2021-06-04 14:44:22 -0500

    Change primitives to valueobjects

--- TodoList/ToDosItems/Application/Create/TodoItemCreator.cs
+++ TodoList/ToDosItems/Application/Create/TodoItemCreator.cs
@@ -14,9 +14,12 @@ namespace TodoList.ToDosItems.Application.Create
         public TodoItemCreator(IToDoRepository conext) => _context = conext;
 
         public async Task CreateToDoItem(int id, string title, string description, bool isDone) {
+            var todoId = new ToDoId(id);
             var todoTitle = new ToDoTitle(title);
+            var todoDescription = new ToDoDescription(description);
+            var todoIsDone = new ToDoIsDone(isDone);
 
-            var toDoItem = new ToDoItem(id, todoTitle, description, isDone);
+            var toDoItem = new ToDoItem(todoId, todoTitle, todoDescription, todoIsDone);
 
             await _context.Save(toDoItem);
         }
