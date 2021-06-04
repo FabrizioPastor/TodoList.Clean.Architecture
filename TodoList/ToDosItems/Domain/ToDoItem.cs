7507017bc98844682616b0a67a57893e8e7f5e55
FMPS2199 <1059712@senati.pe>
2021-06-04 14:45:31 -0500

    Replace props primitive types to valueobjects

--- TodoList/ToDosItems/Domain/ToDoItem.cs
+++ TodoList/ToDosItems/Domain/ToDoItem.cs
@@ -9,12 +9,12 @@ namespace TodoList.ToDosItems.Domain
 {
     public class ToDoItem : AggregateRoot
     {
-        public int TodoItemId { get; private set; }
+        public ToDoId TodoItemId { get; private set; }
         public ToDoTitle Title { get; private set; }
-        public string Description { get; private set; }
-        public bool IsDone { get; private set; }
+        public ToDoDescription Description { get; private set; }
+        public ToDoIsDone IsDone { get; private set; }
 
-        public ToDoItem(int id, ToDoTitle title, string description, bool isDone) {
+        public ToDoItem(ToDoId id, ToDoTitle title, ToDoDescription description, ToDoIsDone isDone) {
             TodoItemId = id;
             Title = title;
             Description = description;
@@ -22,30 +22,34 @@ namespace TodoList.ToDosItems.Domain
         }
 
         public void CheckAsCompleted() {
-            IsDone = true;
+            IsDone = new ToDoIsDone(true);
         }
 
-        public static ToDoItem Create(int id, ToDoTitle title, string description) {
+        public void TogleCompleted() {
+            IsDone = new ToDoIsDone(!IsDone.Value);
+        }
+
+        public static ToDoItem Create(ToDoId id, ToDoTitle title, ToDoDescription description) {
             //TODO: Implement domain events
-            return new ToDoItem(id, title, description, false);
+            return new ToDoItem(id, title, description, new ToDoIsDone(false));
         }
 
         public Dictionary<string, object> ToPrimitive() {
             return new Dictionary<string, object>
             {
-                { "TodoItemId", TodoItemId },
+                { "TodoItemId", TodoItemId.Value },
                 { "Title", Title.Value },
-                { "Description", Description },
-                { "IsDone", Description }
+                { "Description", Description.Value },
+                { "IsDone", IsDone.Value }
             };
         }
         
         public static ToDoItem FromPrimitive(Dictionary<string, object> primitive) {
             return new ToDoItem(
-                Convert.ToInt32(primitive["TodoItemId"]),
+                new ToDoId(Convert.ToInt32(primitive["TodoItemId"])),
                 new ToDoTitle(Convert.ToString(primitive["Title"])),
-                Convert.ToString(primitive["Description"]),
-                Convert.ToBoolean(primitive["IsDone"])
+                new ToDoDescription(Convert.ToString(primitive["Description"])),
+                new ToDoIsDone(Convert.ToBoolean(primitive["IsDone"]))
                 );
         }
     }
