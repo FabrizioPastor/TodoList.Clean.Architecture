aab3f88302ca6cbea4c09c142c47e037a4782a2e
FMPS2199 <1059712@senati.pe>
2021-06-04 19:18:51 -0500

    change model builder for value objects

--- TodoList/ToDosItems/Shared/Infraestrucutre/Persistence/EntityFramework/EntityConfigurations/ToDoItemConfiguration.cs
+++ TodoList/ToDosItems/Shared/Infraestrucutre/Persistence/EntityFramework/EntityConfigurations/ToDoItemConfiguration.cs
@@ -11,26 +11,52 @@ namespace TodoList.ToDosItems.Shared.Infraestrucutre.Persistence.EntityFramework
 {
     public class ToDoItemConfiguration : IEntityTypeConfiguration<ToDoItem>
     {
-        public void Configure(EntityTypeBuilder<ToDoItem> builder)
+        public void Configure(EntityTypeBuilder<ToDoItem> builder) 
         {
-            builder.ToTable("todo_item").OwnsOne(t => t.TodoItemId)
-                .HasKey(v => v.Value);
+            builder.ToTable("todo_item");
 
-            builder.OwnsOne(t => t.TodoItemId)
-                .Property(v => v.Value)
-                .HasField("todo_id");
+            builder.OwnsOne(t => t.TodoItemId, a =>
+            {
+                a.HasKey(v => v.Value);
+                a.Property(v => v.Value)
+                    .HasColumnName("todo_id");
+            });
+                
 
-            builder.OwnsOne(t => t.Title)
-                .Property(v => v.Value)
-                .HasField("title");
+            // builder.OwnsOne(t => t.TodoItemId)
+            //     .Property(v => v.Value)
+            //     .HasField("todo_id");
 
-            builder.OwnsOne(t => t.Description)
-                .Property(v => v.Value)
-                .HasField("description");
+            builder.OwnsOne(t => t.Title, a =>
+            {
+                a.Property(v => v.Value)
+                    .HasColumnName("title");
+            });
+            
+            // builder.OwnsOne(t => t.Title)
+            //     .Property(v => v.Value)
+            //     .HasField("title");
 
-            builder.OwnsOne(t => t.IsDone)
-                .Property(v => v.Value)
-                .HasField("is_done");
+            builder.OwnsOne(t => t.Description, a =>
+            {
+                a.Property(v => v.Value)
+                    .HasColumnName("description");
+            });
+            
+            // builder.OwnsOne(t => t.Description)
+            //     .Property(v => v.Value)
+            //     .HasField("description");
+
+            builder.OwnsOne(t => t.IsDone, a =>
+            {
+                a.Property(v => v.Value)
+                    .HasColumnName("is_done");
+            });
+            
+            // builder.OwnsOne(t => t.IsDone)
+            //     .Property(v => v.Value)
+            //     .HasField("is_done");
+
         }
     }
 }
