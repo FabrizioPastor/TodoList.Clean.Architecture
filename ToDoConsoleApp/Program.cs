8ba03ff2b63b152c8d21821cc86bd310a3e1e717
FMPS2199 <1059712@senati.pe>
2021-06-06 12:31:12 -0500

    Add consoleApp

---
+++ ToDoConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using TodoList.ToDosItems.Application.SearchAll;
+using TodoList.ToDosItems.Infraestructure.Persistence;
+using TodoList.ToDosItems.Shared.Infraestrucutre.Persistence.EntityFramework;
+
+namespace ToDoConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                
+                var repository = new MySqlToDoItemRepository(new TodoListContext());
+                var toDoSearcher = new ToDoItemSearcher(repository);
+                var response = await toDoSearcher.SearchAllToDoItems();
+                Console.WriteLine("Message from console");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+
+    public enum Repository { 
+        MySql,
+        InMemory,
+        SqlServer
+    }
+}
