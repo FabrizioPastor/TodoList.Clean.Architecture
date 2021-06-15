c7c8f6dbddbcc3ed3be8fcfcc63a29e1cf154b62
FMPS2199 <1059712@senati.pe>
2021-06-15 18:53:11 -0500

    Add domain events to AggregateRoot.cs

--- Shared/Domain/AggregateRoot/AggregateRoot.cs
+++ Shared/Domain/AggregateRoot/AggregateRoot.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Shared.Domain.Bus.Event;
 
 namespace Shared.Domain.AggregateRoot
 {
     public class AggregateRoot
     {
-        
+        private List<DomainEvent> _domainEvents = new List<DomainEvent>();
+
+        public List<DomainEvent> PullDomainEvents()
+        {
+            var events = _domainEvents;
+
+            _domainEvents = new List<DomainEvent>();
+
+            return events;
+        }
+
+        protected void Record(DomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
     }
 }
