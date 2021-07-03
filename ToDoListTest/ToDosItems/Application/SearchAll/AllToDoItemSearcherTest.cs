7edc66fb3664129092042785ec6db120cbca2d2e
FMPS2199 <1059712@senati.pe>
2021-07-03 13:24:35 -0500

    add AllToDoSearchDapper test

--- ToDoListTest/ToDosItems/Application/SearchAll/AllToDoItemSearcherTest.cs
+++ ToDoListTest/ToDosItems/Application/SearchAll/AllToDoItemSearcherTest.cs
@@ -6,6 +6,7 @@ using TodoList.ToDosItems.Application.Create;
 using TodoList.ToDosItems.Application.SearchAll;
 using TodoList.ToDosItems.Domain;
 using TodoList.ToDosItems.Infraestructure.Persistence;
+using TodoList.ToDosItems.Infraestructure.Persistence.Dapper;
 using TodoList.ToDosItems.Shared.Infraestrucutre.Persistence.EntityFramework;
 
 namespace ToDoListTest.ToDosItems.Application.SearchAll
@@ -14,9 +15,18 @@ namespace ToDoListTest.ToDosItems.Application.SearchAll
     public class AllToDoItemSearcherTest
     {
         [TestMethod]
-        public async Task AllToDoSearch()
+        public async Task AllToDoSearchEf()
         {
             var repository = new MySqlToDoItemRepository(new TodoListContext());
+            var useCase = new AllToDoItemSearcher(repository);
+            var result = await useCase.SearchAllToDoItems();
+            Assert.IsNotNull(result); 
+        }
+        
+        [TestMethod]
+        public async Task AllToDoSearchDapper()
+        {
+            var repository = new MySqlToDoItemDapperRepository();
             var useCase = new AllToDoItemSearcher(repository);
             var result = await useCase.SearchAllToDoItems();
             Assert.IsNotNull(result); 
